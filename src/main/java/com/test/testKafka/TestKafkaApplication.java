198ecf12a5467bf2f496608f9348dad9d73e5ff0
chenguoku <chenguoku@xforceplus.com>
2019-01-02 14:16:01 +0800

    测试kafka 第一次提交

---
+++ src/main/java/com/test/testKafka/TestKafkaApplication.java
@@ -0,0 +1,14 @@
+package com.test.testKafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestKafkaApplication.class, args);
+	}
+
+}
+
